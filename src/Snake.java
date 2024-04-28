8dd9fd2353e7e8e6e5a55be81a6d1c189429fc6b
Teresa <tbaptista435@gmail.com>
2024-04-28 15:55:02 +0100

    Adding KeyboardLogic

--- src/Snake.java
+++ src/Snake.java
@@ -3,9 +3,10 @@ public class Snake {
     public static void main(String[] args) throws InterruptedException {
 
         Game game = new Game(50,50,150);
-
+        Player player = new Player();
         game.init();
         game.start();
+        KeyboardLogic keyboardLogic = new KeyboardLogic();
     }
 }
 
